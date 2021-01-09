a3e3104676fa570712078cdc1966adf8be8fbff4
T-unity <takadanaritoshi@gmail.com>
2021-01-09 23:32:36 +0900

    FizzBuzzプログラム

--- 1_Fizzbuzz.rb
+++ 1_Fizzbuzz.rb
@@ -1,9 +1,23 @@
-puts 'ちゃんと動いてるか確認やで。'
+puts 'Hello'
 
 # f_b(Fizz_Buzz)
 def f_b(n)
-  n.to_s
+  if n%15==0
+    'FizzBuzz'
+  elsif n%3==0
+    'Fizz'
+  elsif n%5==0
+    'Buzz'
+  else
+    n.to_s
+  end
 end
 
 puts f_b(1)
 puts f_b(2)
+puts f_b(3)
+puts f_b(4)
+puts f_b(5)
+puts f_b(6)
+puts f_b(15)
+puts f_b(30)
