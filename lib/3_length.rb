95ab5c15d972a57170324ce948421eca83e38a45
T-unity <takadanaritoshi@gmail.com>
2021-01-15 19:01:26 +0900

    位置No.4585

--- lib/3_length.rb
+++ lib/3_length.rb
@@ -8,3 +8,14 @@ def convert_length(length,unit_form,unit_to)
   units = {m: 1.0, ft: 3.28, in: 39.37}
   (length / units[unit_form] * units[unit_to]).round(2)
 end
+
+def convert_length(length,from: :m,to: :m)
+  units = {m: 1.0, ft: 3.28, in: 39.37}
+  (length / units[from] * units[to]).round(2)
+end
+
+# キーや値が変わらないハッシュ（オブジェクト）はメソッド外で定数として保持した方が実行効率がよくなる
+UNITS = {m: 1.0, ft: 3.28, in: 39.37}
+def convert_length(length,from: :m,to: :m)
+  (length / UNITS[from] * UNITS[to]).round(2)
+end
--- lib/3_method.rb
+++ lib/3_method.rb
@@ -28,3 +28,19 @@ puts currency[:ja]
 puts 'refactoring'
 currency = { ja:'yen', us:'dollar', italy:'euro' }
 puts currency
+
+puts 'keys method'
+currency = { ja:'yen', us:'dollar', italy:'euro' }
+puts currency.keys
+puts '-----'
+puts currency.values
+puts '-----'
+puts currency.has_key?(:ja)
+puts currency.has_key?(:mongol)
+puts 'hash to_a'
+change = currency.to_a
+puts change
+
+a = 'foo'
+puts a&.upcase
+# puts b&.upcase
