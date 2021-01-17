33e9de5a257a13e9b859ed5e10a4e47c1f48d97b
T-unity <takadanaritoshi@gmail.com>
2021-01-17 10:36:18 +0900

    位置No.6045

--- lib/5/method.rb
+++ lib/5/method.rb
@@ -90,3 +90,83 @@ puts user.hello
 
 user2 = User.new("Jin")
 puts user2.hello
+
+
+class User
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def hello
+    "Hello, I am #{name}"
+  end
+
+  def hi
+    "Hi, I am #{self.name}"
+  end
+
+  def my_name
+    "My name is #{@name}"
+  end
+end
+
+puts '以下の3つは全て同じ文字列を返す'
+user = User.new('Alice')
+puts user.hello
+puts user.hi
+puts user.my_name
+
+puts 'selfを付けなければ不具合が発生する場合'
+class User
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def rename_to_bob
+    name = 'Bob'
+  end
+
+  def rename_to_jon
+    self.name = 'Jon'
+  end
+
+  def rename_to_gen
+    @name = 'Gen'
+  end
+end
+
+user = User.new('Alice')
+user.rename_to_bob
+puts user.name
+user.rename_to_jon
+puts user.name
+user.rename_to_gen
+puts user.name
+
+class Foo
+  puts "クラス構文の直下のself:#{self}"
+
+  def self.bar
+    puts "クラスメソッド内のself:#{self}"
+  end
+
+  def baz
+    puts "インスタンスメソッド内のself:#{self}"
+  end
+end
+
+puts Foo.bar
+
+foo = Foo.new
+foo.baz
+
+puts User.superclass
+
+class Gamer < User
+end
+
+puts Gamer.superclass
