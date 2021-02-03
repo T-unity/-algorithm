9996aad8e59ab151a184c31504f20ba50d0d55bc
T-unity <takadanaritoshi@gmail.com>
2021-02-03 20:19:06 +0900

    paiza

--- paiza/paiza_Drank.rb
+++ paiza/paiza_Drank.rb
@@ -1,5 +1,25 @@
 # -------------------
 n = gets.split(' ').map(&:to_i)
+
+x = n[0].to_f
+y = n[1].to_f
+z = n[2].to_f
+
+i1 = x - (x * (y / 100))
+puts i1
+i2 = i1 - (i1 * (z / 100))
+puts i2
+
+# -------------------
+n = gets.to_i
+if n >=40 && n <= 60
+  puts 'OK'
+else
+  puts 'NG'
+end
+
+# -------------------
+n = gets.split(' ').map(&:to_i)
 n1 = n[0]
 n2 = n[1]
 
@@ -16,7 +36,6 @@ else
 end
 
 puts i1 + i2
-
 
 # -------------------
 n = gets.split(' ').map(&:to_i)
