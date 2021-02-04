f26c8840e936c652b1a7227de759c2be69d70b61
T-unity <takadanaritoshi@gmail.com>
2021-02-04 22:08:43 +0900

    skill

--- ruby/Drank.rb
+++ ruby/Drank.rb
@@ -1,14 +1,4 @@
-# -------------------
-n = gets.split(' ').map(&:to_i)
 
-x = n[0].to_f
-y = n[1].to_f
-z = n[2].to_f
-
-i1 = x - (x * (y / 100))
-puts i1
-i2 = i1 - (i1 * (z / 100))
-puts i2
 
 # -------------------
 n = gets.to_i
